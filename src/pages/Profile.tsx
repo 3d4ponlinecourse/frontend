e7f932602f1a0aca84af5ab320701b871d01037b
AK747 <bhusingha.saimongkol@gmail.com>
2023-07-18 11:01:25 +0700

    Add : Profile page & component

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const Profile = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <img></img>
+                <h2></h2>
+            </div>
+                <h3></h3>
+                <h3></h3>
+                <h3></h3>
+                <h3></h3>
+            <div>
+            </div>
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
