b876136569e9a1c5f740c0077b1bfda1f54eb6ee
Paweena937 <134252989+Paweena937@users.noreply.github.com>
2023-07-19 15:31:17 +0700

    add edit profile new

--- src/components/Editprofile.tsx
+++ src/components/Editprofile.tsx
@@ -2,21 +2,21 @@
 
 const Editprofile = () => {
   return (
-   <div>
-    <body className="bg-teal-950">
-    <div className="flex place-content-center bg-zinc-600">For Profile Content
-    <div className="font bold text-xl">Edit Your Profile here
-        <div className="grid gap-3 mb-6 text-base">
+    <div className="flex flex-col gap-2" >
+    
+    <div className="text-center font-bold text-xls">Edit Your Profile here
+        <div className="flex flex-col gap-6 rounded-xl my-14 py-5 px-7 indent-1">
         <input  className=''type="text"  placeholder="Firstname" />
         <input type="text"  placeholder="Lastname"/>
         <input type="email" placeholder="Email" />
             </div>
-            <div className="bg-teal-400 hover:bg-teal-950 text-white font-bold py-2 rounded-full indent-2">
+
+
+            <div className="bg-teal-400 hover:bg-teal-950 text-center text-white font-bold py-2 rounded-full indent-2 my-0.5">
             <button>Save Change</button>
-      </div>   
+        
 </div>
 </div>
-</body>
 </div>
 
   )
