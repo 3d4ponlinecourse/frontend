9335f27489e99f5f18c0b1696f7565470ea59983
Paweena937 <134252989+Paweena937@users.noreply.github.com>
2023-07-26 16:53:44 +0700

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
+import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import React from 'react'
 import { useAuth } from './providers/AuthProvider'
+import { Profile } from './pages/Profile'
 import Navbar from './components/Navbar'
 import GuardedRoute from './guards/GuardedRoute'
-import Login from './pages/Login'
-import Register from './pages/Register'
 import Home from './pages/Home'
-import Profile from './pages/Profile'
-import Create from './pages/Create'
-import ContentCourse from './pages/ContentCourse'
+import Register from './pages/Register'
+
+
 
 function App() {
   const { isLoggedIn } = useAuth()
@@ -17,18 +16,13 @@ function App() {
   return (
     <div>
       <Navbar />
+      <Home />
       <Routes>
         <Route element={<GuardedRoute isRouteAccessible={!isLoggedIn} redirectRoute="/" />}>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/contentcourse" element={<ContentCourse />} />
-
-          <Route path="/" element={<Home />} />
         </Route>
-        <Route element={<GuardedRoute isRouteAccessible={isLoggedIn} redirectRoute="/" />}>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create" element={<Create />} />
-        </Route>
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   )
