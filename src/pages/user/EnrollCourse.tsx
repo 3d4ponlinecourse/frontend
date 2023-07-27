4a03047d208005e2909be88a2a2b95e6fbc2326a
Paweena937 <134252989+Paweena937@users.noreply.github.com>
2023-07-27 11:17:53 +0700

    add enroll

--- src/pages/user/EnrollCourse.tsx
+++ src/pages/user/EnrollCourse.tsx
@@ -1,25 +1,5 @@
 import React from 'react'
 
-interface Course {
-  courseName: string
-  videoUrl: string
-  duration: string
-  description: string
-}
-
-interface User {
-  id: number
-  userId: string
-  courseId: number
-  courseName: string
-  enrolledCourses: Course[]
-}
-
-const users: User[] = []
-function addCourse(courses: Course): void {
-  push(courses)
-}
-
 const EnrollCourse = () => {
   return <div>EnrollCourse</div>
 }
