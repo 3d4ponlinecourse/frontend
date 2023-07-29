79289967fb2614c6a1a5fb395788279d6adc401a
Shin <58554493+shinchinshin@users.noreply.github.com>
2023-07-29 17:45:02 +0700

    update: useCourse hook

--- src/hooks/useCourse.tsx
+++ src/hooks/useCourse.tsx
@@ -1,29 +1,30 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
+import { ShowCourse } from "../types/course";
 
-const useCourse = (id: string) => {
-  const [course, setCourse] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(null)
+const useCourse = (courseId: number) => {
+  const [course, setCourse] = useState<ShowCourse[] | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsLoading(true)
+      setIsLoading(true);
       try {
-        const res = await fetch(`http://localhost:8000/course/${id}`)
-        const data = await res.json()
+        const res = await fetch(`http://localhost:8000/course/${courseId}`);
+        const data = await res.json();
 
-        setCourse(data)
+        setCourse(data);
       } catch (err: any) {
-        setError(err.message)
+        setError(err.message);
       } finally {
-        setIsLoading(false)
+        setIsLoading(false);
       }
-    }
+    };
 
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
-  return { course, isLoading, error }
-}
+  return { course, isLoading, error };
+};
 
-export default useCourse
+export default useCourse;
