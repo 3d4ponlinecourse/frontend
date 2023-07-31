ff65fa2b3cd2375d964ffec19467a1f0cdc384bd
AK747 <bhusingha.saimongkol@gmail.com>
2023-07-31 23:28:35 +0700

    git cache clear

--- src/components/profile/CurrentCourse.tsx
+++ src/components/profile/CurrentCourse.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-
+import CurrentCourseList from './CurrentCourseList'
 import { IUserWithEnrollment } from '../../types/enrollment'
 import Nonenroll from './Nonenroll'
-import CurrentCourseList from './CurrentCourseList'
 
 export interface ICurrentCourseProps {
   user: IUserWithEnrollment
@@ -13,14 +12,13 @@ const CurrentCourse = ({ user }: ICurrentCourseProps) => {
     <div className="">
       <div className="flex justify-between items-center">
         <p className="text-3xl font-bold">Lasted Courses</p>
+        <button className="text-s font-medium">SEE ALL</button>
       </div>
       <div>
         {user.enrollment.length === 0 ? (
           <Nonenroll />
         ) : (
-          user.enrollment.map((enrollment) => (
-            <CurrentCourseList key={(enrollment.courseName, enrollment.courseId)} enrollment={enrollment} />
-          ))
+          user.enrollment.map((enrollment) => <CurrentCourseList key={enrollment.courseName} enrollment={enrollment} />)
         )}
       </div>
     </div>
